790bc0f403908e973ba9170cf9f668c890416f3c
Hiroki Hirabayashi <hh.38063806@gmail.com>
2020-06-23 23:19:56 +0900

    ナビゲーションコントローラ配置 Login画面の雛形を作成しました。

---
+++ Swift5 Like&Heart/Controller/LoginViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LoginViewController.swift
+//  Swift5 Like&Heart
+//
+//  Created by 平林宏淳 on 2020/06/23.
+//  Copyright © 2020 Hiroaki_Hirabayashi. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
