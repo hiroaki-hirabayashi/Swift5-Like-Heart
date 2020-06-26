7255ae6a518a966a707a93ff477104c31b0e9dcd
Hiroki Hirabayashi <hh.38063806@gmail.com>
2020-06-26 11:21:04 +0900

    画像が選択された時、キャンセル時のメソッド追加

--- Swift5 Like&Heart/Controller/EditViewController.swift
+++ Swift5 Like&Heart/Controller/EditViewController.swift
@@ -36,6 +36,11 @@ class EditViewController: UIViewController, UIImagePickerControllerDelegate, UIN
         }
     }
     
+    //imageViewをタップ出来る様にする　チェックを入れ、tapを配置
+    @IBAction func tapImageView(_ sender: Any) {
+        openActionSheet()
+    }
+    
     func openActionSheet() {
         let alert: UIAlertController = UIAlertController(title: "選択してください。", message: "", preferredStyle: .actionSheet)
         
@@ -86,6 +91,61 @@ class EditViewController: UIViewController, UIImagePickerControllerDelegate, UIN
     
     
     @IBAction func done(_ sender: Any) {
+        
+        
+        
+        performSegue(withIdentifier: "timeLine", sender: nil)
+    }
+    
+    func sendAndGetImageURL() {
+        /*
+         DB: https://swift5-like-and-heart.firebaseio.com/
+         Storage: gs://swift5-like-and-heart.appspot.com/
+         */
+        
+        let ref = Database.database().reference(fromURL: "https://swift5-like-and-heart.firebaseio.com/")
+        let storage = Storage.storage().reference(forURL: "gs://swift5-like-and-heart.appspot.com/")
+        //画像が入るファイルを作成してそこに入れる
+        //名前も決める
+        let key = ref.childByAutoId().key
+        let imageRef = storage.child("ProfileImage").child("\(key).jpeg")
+        
+        var imageData: Data = Data()
+        //self.imageView.imageがnilじゃなかったら
+        if self.imageView.image != nil {
+            imageData = (self.imageView.image?.jpegData(compressionQuality: 0.03)) as! Data
+
+        //アップロード
+            let uploadTask = imageRef.putData(imageData, metadata: nil) { (metadata, error) in
+                if error != nil {
+                    print(error as Any)
+                    return
+                }
+                imageRef.downloadURL { (url, error) in
+                    if url != nil {
+                        //HUD
+                        self.imageURL = url
+                        UserDefaults.standard.set(self.imageURL?.absoluteString, forKey: "ProfileImageString")
+                    }
+                }
+            }
+            uploadTask.resume()
+
+        }
+        
+        //キャンセル
+        func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+            picker.dismiss(animated: true, completion: nil)
+        }
+        
+        //画像が選択されたら
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+           //info[.editedImage]がUIImageとしてpickedImageに入っていたら
+            if let pickedImage = info[.editedImage] as? UIImage {
+                self.imageView.image = pickedImage
+                picker.dismiss(animated: true, completion: nil)
+            }
+        }
     }
     
 
